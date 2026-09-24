3526a41330cc5d91d01ad7d7c21eb95c58b28acb
Felipe-gsilva <felipe.gsilva@protonmail.com>
2026-09-24 15:06:44 -0300

    markdown-preview-fix

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,11 +1,6 @@
 -- [nfnl] fnl/plugins/markdown-preview.fnl
 local function _1_()
-  local function _2_()
-  end
-  return vim.fn["mkdp#util#install"][_2_]
-end
-local function _3_()
   vim.g.mkdp_filetypes = {"markdown"}
   return nil
 end
-return {{"iamcco/markdown-preview.nvim", cmd = {"MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop"}, build = _1_, init = _3_, ft = {"markdown"}}}
+return {{"iamcco/markdown-preview.nvim", cmd = {"MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop"}, build = "cd app && npm install", init = _1_, ft = {"markdown"}}}
